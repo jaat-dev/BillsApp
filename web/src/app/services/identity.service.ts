0acd7df4455f0b67b3e69aac360be387ddf93e70
John Arévalo <jaat.dev@hotmail.com>
2022-04-14 15:16:39 -0500

    otro

--- web/src/app/services/identity.service.ts
+++ web/src/app/services/identity.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { LoginModel } from "../components/login/models/login.model";
 import { Observable } from 'rxjs';
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class IdentityService {
   constructor(private http:HttpClient) { }
 
   signIn(model:LoginModel): Observable<any> {
+    debugger
     return  this.http.post('`${environment.apiUrl}sign-in', model);
   }
 }
